1aa14d8ef56fdd708a200623504a7ec56a50f5f0
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-06 13:02:34 +0200

    act12

---
+++ act12.py
@@ -0,0 +1,5 @@
+barayer=int(input("Número de barras vendidas: "))
+pan=float(3.49)
+dia=float(0.60)
+print("El total a pagar es de",pan,"pero con el descuento del",dia,"es de:", 
+      round((pan*dia)*barayer,2))
