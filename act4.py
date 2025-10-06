157f46db60f55a6ded60c3a1a0684d78c406e314
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-06 12:22:00 +0200

    act 5

--- act4.py
+++ act4.py
@@ -1,2 +1,2 @@
-resultado = (3 + 2 / 2 * 5) ** 2
+resultado = ((3 + 2) / (2 * 5)) ** 2
 print("El resultado de (3+2/2*5)²:", resultado)
---
+++ act5.py
@@ -0,0 +1,3 @@
+horas= input("Ingrese las horas trabajadas: ")
+coste= 11
+print ("Ganarás:", int(horas) * coste)
