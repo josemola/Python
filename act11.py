62cf00d21d025bf3a9a08be9790581f8b6372af9
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-06 12:52:07 +0200

    act11

---
+++ act11.py
@@ -0,0 +1,10 @@
+dinero=float(input("Cuanto dinero tienes: "))
+interes=(0.04*dinero)
+dinero=dinero+interes
+print("El interés que ganarás en un año es de:", round(interes,2))
+interes=(0.04*dinero)
+dinero=dinero+interes
+print("El interés que ganarás en dos años es de:", round(interes,2))
+interes=(0.04*dinero)
+dinero=dinero+interes
+print("El interés que ganarás en tres años es de:", round(interes,2))
