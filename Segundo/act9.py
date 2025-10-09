f6a261b498a885d656b5fb8cf3acc93047afae2f
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-09 10:58:11 +0200

    actividad 9 pt2

---
+++ Segundo/act9.py
@@ -0,0 +1,4 @@
+nacimiento= input("Introduce una fecha de nacimiento en formato dd/mm/aaaa: ")
+print("El usuario nació el día", nacimiento.split("/")[0],
+       "del mes", nacimiento.split("/")[1],
+         "del año", nacimiento.split("/")[2])
