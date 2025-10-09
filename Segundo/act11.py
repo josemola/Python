bb1c41ec904fc56daf767803353ceb4d61cb68d9
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-09 11:13:36 +0200

    act 11 pt2

---
+++ Segundo/act11.py
@@ -0,0 +1,4 @@
+producto=input("Introduce el nombre del producto: ")
+precio=input("Introduce el precio del producto: ")
+unidades=input("Introduce las unidades del producto: ")
+print("Tenemos el producto",producto, "que cuesta", precio, "euros y tenemos", unidades, "unidades, el coste total es de", int(precio)*int(unidades), "euros")
