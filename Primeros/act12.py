20cc84d83b41913a6e40a472218788929f26009f
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-09 10:30:48 +0200

    act12 pt1 cambio %de 60 a 40

--- Primeros/act12.py
+++ Primeros/act12.py
@@ -1,5 +1,5 @@
 barayer=int(input("Número de barras vendidas: "))
 pan=float(3.49)
-dia=float(0.60)
+dia=float(0.40)
 print("El total a pagar es de",pan,"pero con el descuento del",dia,"es de:", 
       round((pan*dia)*barayer,2))
