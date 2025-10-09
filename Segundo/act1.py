2e615c317509d70173e0ada3a2a71d09362abd84
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-09 10:04:40 +0200

    act 1, las demas estan vacias

--- Segundo/act1.py
+++ Segundo/act1.py
@@ -1,5 +1,3 @@
 usuario= input("Ingrese su nombre de usuario: ")
 numero= int(input("Ingrese un número entero: "))
-while numero != 0:
-    print(usuario)
-    numero= numero - 1
+print((usuario +"\n")*numero)
---
+++ Segundo/act2.py
@@ -0,0 +1,5 @@
+nombre= input("Ingrese su nombre completo: ")
+x= 3
+while x != 0:
+    print(nombre)
+    x= x - 1
