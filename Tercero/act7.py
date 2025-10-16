9f0b892fdbdad29d37bf1f5445e4e1463c3f5c2b
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-16 11:56:53 +0200

    act7 esfe

--- Tercero/act7.py
+++ Tercero/act7.py
@@ -1,11 +1,11 @@
-renta=input("Introduce tu renta anual: ")
+renta=float(input("Introduce tu renta anual: "))
 if renta<10000:
-    print("Tipo impositivo del 5%, debes pagar",renta*0.05,"euros")
+    print("Tipo impositivo del 5%, debes pagar",round(renta*0.05,2),"euros")
 elif renta>10000:
-    print("Tipo impositivo del 5%, debes pagar",renta*0.15,"euros")
+    print("Tipo impositivo del 5%, debes pagar",round(renta*0.15,2),"euros")
 elif renta>20000:
-    print("Tipo impositivo del 20%, debes pagar",renta*0.20,"euros")
+    print("Tipo impositivo del 20%, debes pagar",round(renta*0.20,2),"euros")
 elif renta>35000:
-    print("Tipo impositivo del 30%, debes pagar",renta*0.30,"euros")
+    print("Tipo impositivo del 30%, debes pagar",round(renta*0.30,2),"euros")
 elif renta>60000:
-    print("Tipo impositivo del 45%, debes pagar",renta*0.45,"euros")
+    print("Tipo impositivo del 45%, debes pagar",round(renta*0.45,2),"euros")
