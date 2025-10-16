603b27dda2608573ac7910d2def342a1176ee55f
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-16 10:51:08 +0200

    sct5 pt 3

---
+++ Tercero/act5.py
@@ -0,0 +1,6 @@
+Edad=input("Introduce tu edad: ")
+ingresos=input("Introduce tus ingresos mensuales: ")
+if int(Edad)<16 or int(ingresos)<=1000:
+    print("No debes tributar")
+else:
+    print("Debes tributar")
