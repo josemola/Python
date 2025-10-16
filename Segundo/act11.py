e24ab2bd3ae687635d9d66a355840df54e5b27c6
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-16 10:10:16 +0200

    act 11 bien hecha

--- Segundo/act11.py
+++ Segundo/act11.py
@@ -1,4 +1,7 @@
 producto=input("Introduce el nombre del producto: ")
-precio=input("Introduce el precio del producto: ")
-unidades=input("Introduce las unidades del producto: ")
-print("Tenemos el producto",producto, "que cuesta", precio, "euros y tenemos", unidades, "unidades, el coste total es de", int(precio)*int(unidades), "euros")
+precio=float(input("Introduce el precio del producto: "))
+unidades=float(input("Introduce las unidades del producto: "))
+total=(precio)*(unidades)
+totalA=f"{total:011.2f}"
+precioA=f"{precio:09.2f}"
+print("Tenemos el producto",producto, "que cuesta", precioA, "euros y tenemos", unidades, "unidades, el coste total es de", totalA, "euros")
