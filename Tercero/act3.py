6e79d555d066ee040be2d32d2c30af1b1be2c1c7
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-16 10:42:27 +0200

    act 3

---
+++ Tercero/act3.py
@@ -0,0 +1,8 @@
+num1=float(input("Introduce el primer número: "))
+num2=float(input("Introduce el segundo número: "))
+
+if num2==0:
+    print ("Error: División por cero")
+else:
+    division=float(num1/num2)
+    print (division)
