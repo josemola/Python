af0a1d9a13befcbcef6530544e78fc72675a7e70
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-23 10:34:42 +0200

    act10 pt3 mas compleja

--- Tercero/act10.py
+++ Tercero/act10.py
@@ -1,8 +1,23 @@
 pizza=input("Hola ¿Quieres una pizza vegetariana?: ")
-if pizza.lower=="si":
+if pizza.lower()=="si":
     print("Los ingredientes son: Pimiento y tofu")
-    ingredientes=input("Elige un ingrediente:")
+    ingredientes=input("Elige un ingrediente con una P o T:")
+    if ingredientes.lower()=="p":
+        ingredientes="Mozzarella, Tomate y Pimiento"
+    elif ingredientes.lower()=="t":
+        ingredientes="Mozzarella, Tomate y Tofu"
+    else:
+        ingredientes="Mozzarella y Tomate"
+    print("Tu pizza vegetariana lleva: "+ingredientes)
 else:
     print("Los ingredientes son: Peperoni, jamón y salmón")
-    ingredientes=input("Elige un ingrediente: ")
-print("Tu pizza no vegetariana lleva: "+ingredientes)
+    ingredientes=input("Elige un ingrediente con una P, J o S: ")
+    if ingredientes.lower()=="p":
+        ingredientes="Mozzarella, Tomate y Peperoni"
+    elif ingredientes.lower()=="j":
+        ingredientes="Mozzarella, Tomate y Jamón"
+    elif ingredientes.lower()=="s":
+        ingredientes="Mozzarella, Tomate y Salmón"
+    else:
+        ingredientes="Mozzarella y Tomate"
+    print("Tu pizza no vegetariana lleva: "+ingredientes)
