305fc7c284dd0cd3c5633d7ceab1de99ad8a270a
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-23 09:55:06 +0200

    act 9 pt3

---
+++ Tercero/act9.py
@@ -0,0 +1,7 @@
+edad=int(input("Introduce tu edad: "))
+if edad>18:
+    print("Debes pagar entrada de 10 euros.")
+elif edad>3:
+    print("Debes pagar entrada de 5 euros.")
+else:
+    print("Tu entrada es gratis.")
