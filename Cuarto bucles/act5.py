b68774982ac31b97c8a4ebd93d5460462f223451
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-24 14:17:13 +0200

    correccion y act 5 pt4

--- Cuarto bucles/act5.py
+++ Cuarto bucles/act5.py
@@ -1,3 +1,11 @@
 inversion=float(input("Dame una cantidad a invertir: "))
 interes=float(input("Dame el interés porcentual anual: "))
 anios=int(input("Dame el número de años: "))
+ganancias=0
+x=0
+while anios>0:
+    x=x+1
+    ganancias=ganancias+((ganancias+inversion)*interes/100)
+    print("Cantidad ganada después de",x,"años:",ganancias)
+    anios-=1
+print("Cantidad total de dinero ganado + invertido después de",x,"años:",inversion+ganancias)
