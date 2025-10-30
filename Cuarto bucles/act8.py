b916100539add50af6255dabe381d7619c3ba1b6
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-30 10:11:44 +0100

    act 8 pt4

--- Cuarto bucles/act8.py
+++ Cuarto bucles/act8.py
@@ -1,6 +1,8 @@
 numero=int(input("Dame un número entero positivo: "))
 num=1
+a=1
 while numero>0:
-    print("1")
-    numero-=1
-    num=1+num
+    print( a )
+    numero= numero-1
+    num=2+num
+    a=str(num) + " " + str(a)
