5a1d2c1e3de5c5bb969d4871ad37797a3e810292
josemola <jsangal0411@ieszaidinvergeles.org>
2025-10-30 10:15:42 +0100

    act 9 pt4

--- Cuarto bucles/act9.py
+++ Cuarto bucles/act9.py
@@ -1,6 +1,6 @@
-numero=int(input("Dame un número entero positivo: "))
-num=1
-while numero>0:
-    print("1")
-    numero-=1
-    num=1+num
+contrasena=input("Dime una contraseña: ")
+respuesta=input("Introduce la contraseña para acceder: ")
+while contrasena != respuesta :
+    print("Te has equivocado, vuelve a intentarlo")
+    respuesta=input("Introduce la contraseña para acceder: ")
+print("Bienvenid@")
