d7f98affb87da2ec30182eee2a1aaa7bc2074599
josemola <jsangal0411@ieszaidinvergeles.org>
2025-11-07 14:09:55 +0100

    act 7 pt5

--- Quinto Listas y tuplas/act7.py
+++ Quinto Listas y tuplas/act7.py
@@ -1,1 +1,9 @@
-abecedario=[a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z]
+abecedario=[ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+cuantas=len(abecedario)
+val=2
+while val<cuantas:
+    zz=abecedario[val]
+    abecedario.remove(zz)
+    val=val+2
+    cuantas=len(abecedario)
+print (abecedario)
