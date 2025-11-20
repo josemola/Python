c87ab8864004f2ec8d30a0598560b847ba41cb43
josemola <jsangal0411@ieszaidinvergeles.org>
2025-11-20 09:38:48 +0100

    act 10 pt5

--- Quinto Listas y tuplas/act10.py
+++ Quinto Listas y tuplas/act10.py
@@ -1,12 +1,12 @@
 lista=[50,75,46,22,80,65,8]
 x=0
 mayor=0
-menor=0
+menor=1000000000
 while x<len(lista):
-    if:
-        mayor=
-        x=x+1
-    elif:
-        menor=
-    lista.remove(lista[x])
+    if lista[x]>mayor:
+        mayor=lista[x]
+    elif lista[x]<menor:
+        menor=lista[x]
     x=x+1
+
+print ("El valor mayor es: ",mayor," y el menor es ",menor)
