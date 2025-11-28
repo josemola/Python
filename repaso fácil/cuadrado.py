3d600db2a8cf75ded619373247378f5401333e5a
josemola <jsangal0411@ieszaidinvergeles.org>
2025-11-28 14:13:17 +0100

    nada importante

---
+++ repaso fácil/cuadrado.py
@@ -0,0 +1,3 @@
+x = int(input("Dime un numero "))
+for i in range (0,x):
+    print ("* "*x)
---
+++ repaso fácil/for.py
@@ -0,0 +1,6 @@
+suma = 0
+for i in range (1,11):
+    num=int(input("Di un número"))
+    suma= suma + num
+print ("la suma total es: ", suma)
+
