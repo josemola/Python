70aa7e0790f53d0a21ed720fa9087f9d18c48e60
josemola <jsangal0411@ieszaidinvergeles.org>
2025-11-28 14:42:01 +0100

    locura matrices todas

--- Quinto Listas y tuplas/act12.py
+++ Quinto Listas y tuplas/act12.py
@@ -1,11 +1,14 @@
 matriz1=[
-    [1,3],
-    [4,5]
+    [1,3,3],
+    [4,5,2]
 ]
 matriz2=[
     [4,2],
+    [5,7],
     [5,7]
 ]
+#No se puede realizar porque faltan las matrices, lo de abajo iba a calcular cualquier matriz
+#pero como no se me pide eso, no lo he terminado.
 res=[]
 x = 0
 y = 0
@@ -13,7 +16,25 @@ xa=0
 ya= 0
 contar=0
 fila=0
-while contar != len(matriz1)*2 :
+print(len(matriz1[1]))
+
+while contar != len(matriz1) :
+    while x != len(matriz1[1]) :
+        while x != len(matriz1[1]) :
+            pt1=matriz1[x][y]*matriz2[xa][ya]
+            suma=pt1+suma
+            x=x+1
+            ya=ya+1
+    res=suma
+    suma=0
+    x=0
+    ya=0
+    xa=xa+1
+xa=0
+contar=contar+1
+
+
+
     y=0
     xa=0
     pt1=matriz1[x][y]*matriz2[xa][ya]
