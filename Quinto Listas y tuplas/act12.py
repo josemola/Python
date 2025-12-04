2e6128aa656e235453c4db0a84becc92b4c29c78
josemola <jsangal0411@ieszaidinvergeles.org>
2025-12-04 10:12:50 +0100

    act 12 calcula todas las matrices que pongas

--- Quinto Listas y tuplas/act12.py
+++ Quinto Listas y tuplas/act12.py
@@ -9,46 +9,34 @@ matriz2=[
 ]
 #No se puede realizar porque faltan las matrices, lo de abajo iba a calcular cualquier matriz
 #pero como no se me pide eso, no lo he terminado.
-res=[]
+# print(matriz1[0][1]) = 3 el del centro
+# print (len(matriz1)) = 2 altura de la matriz
+# print (len(matriz1[0])) = 3 ancho de la matriz
+
+suma=0
+res=[[]]
 x = 0
 y = 0
 xa=0
 ya= 0
 contar=0
 fila=0
-print(len(matriz1[1]))
-
-while contar != len(matriz1) :
-    while x != len(matriz1[1]) :
-        while x != len(matriz1[1]) :
-            pt1=matriz1[x][y]*matriz2[xa][ya]
+while contar != len(matriz1) : # calcula por fila en este caso 2
+    while xa != len(matriz2[0]) : # calcula la columna de la 2ª en este caso 2
+        while x < len(matriz1[0]) :  # recorre todos los elementos k (0..cols-1)
+            pt1=matriz1[y][x]*matriz2[x][xa]  # matriz1[row][k] * matriz2[k][col]
             suma=pt1+suma
             x=x+1
-            ya=ya+1
-    res=suma
-    suma=0
-    x=0
-    ya=0
-    xa=xa+1
-xa=0
-contar=contar+1
-
-
-
-    y=0
-    xa=0
-    pt1=matriz1[x][y]*matriz2[xa][ya]
+        x=0
+        ya=0
+        res[contar].append(suma)
+        xa=xa+1
+        suma=0
+    if contar+1 != len(matriz1):
+        res.append([])
     y=y+1
-    xa=xa+1
-    pt2=matriz1[x][y]*matriz2[xa][ya]
-    resultado=pt1+pt2
-    if ya == 0 :
-        ya=ya+1
-    else:
-        ya = 0
-        x = x+1
-    anadir=res.append(resultado[fila][contar])
+    xa=0
     contar=contar+1
-    if contar == len(matriz1):
-        fila=fila+1
-    print (resultado)
+
+for fila in res:
+    print(fila)
