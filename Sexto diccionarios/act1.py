f5f4c4675fb1665a1609de7e7bfe8094bb8f6fa8
josemola <jsangal0411@ieszaidinvergeles.org>
2025-12-12 14:33:39 +0100

    act1 pt6

---
+++ Sexto diccionarios/act1.py
@@ -0,0 +1,8 @@
+diccionario={'Euro':'€','Dollar':'$','Yen':'¥'}
+divisa=input("Dime una divisa: ")
+if divisa.title() in diccionario:
+    print(diccionario[divisa.title()])
+else:
+    print("Eso no es válido")
+
+
