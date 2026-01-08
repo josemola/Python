fd994901ad489a2b284ca88c6aa32b0e5a4bb299
josemola <jsangal0411@ieszaidinvergeles.org>
2026-01-08 10:01:04 +0100

    act 3 pt6

--- Sexto diccionarios/act3.py
+++ Sexto diccionarios/act3.py
@@ -1,6 +1,7 @@
 diccionario={'Plátano': 1.35,'Manzana': 0.80,'Pera': 0.85, 'Naranja' : 0.70}
 nombre = input("Dime una fruta: ")
+kgs = float(input("Dime cuantos kilos quieres: "))
 if nombre.title() in diccionario:
-    print(diccionario[nombre.title()])
+    print("El precio de ", nombre, " es ", float(diccionario[nombre.title()])*kgs)
 else :
     print ("La fruta no existe")
