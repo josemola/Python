b39c3a3867ac7d71f3bed37244efa12dbb6e87ce
josemola <jsangal0411@ieszaidinvergeles.org>
2026-01-09 14:08:18 +0100

    act 6 pt6

---
+++ Sexto diccionarios/act6.py
@@ -0,0 +1,8 @@
+diccionario={}
+termina = "no"
+while termina != "si":
+    clase=input("Dame una clase a la que pertenece tu dato")
+    info=input("Dame la información de esa clase de dato")
+    termina=input("¿Has terminado?")
+    diccionario[clase]=info
+    print(diccionario)
