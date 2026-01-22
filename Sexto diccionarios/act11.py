dc109be0906ee745489520a39786b8563b09b667
josemola <jsangal0411@ieszaidinvergeles.org>
2026-01-22 09:45:48 +0100

    act 11 pt6

--- Sexto diccionarios/act11.py
+++ Sexto diccionarios/act11.py
@@ -1,23 +1,23 @@
 frase="nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
 clientes={}
-x=1
-a=0
 
-print(len(frase.split(";")))
+filas = frase.split("\n")
 
-while x < len(frase):
-    nif= print(frase.split(";")[5+a])
-    nombre= print(frase.split(";")[6+a])
-    email= print(frase.split(";")[7+a])
-    telefono= print(frase.split(";")[8+a])
-    descuento= print(frase.split(";")[9+a])
+for i in range(1, len(filas)):
+    datos_fila = filas[i].split(";")
+    nif = datos_fila[0]
+    nombre = datos_fila[1]
+    email = datos_fila[2]
+    telefono = datos_fila[3]
+    descuento = datos_fila[4]
+    
+    datos = {"nombre": nombre,
+             "email": email,
+             "teléfono": telefono,
+             "descuento": descuento}
+    
+    clientes[nif] = datos
 
-    datos={"nombre":nombre,
-    "email":email,
-    "teléfono":telefono,
-    "descuento":descuento,}
-    x=x+1
-    a=a+5
-    clientes[nif]=datos
+print(clientes)
 
 
