67fc28c29beee7a84998dde4f5c049ce4fe87c61
josemola <jsangal0411@ieszaidinvergeles.org>
2026-01-22 11:11:00 +0100

    act 8 pt7 no se hace

---
+++ Septimo funciones/act8.py
@@ -0,0 +1,10 @@
+def cuadrado(lista):
+    total = []
+    for numero in lista:
+        num = numero ** 2
+        total.append(num)
+    return total
+
+print(cuadrado([1, 2, 2, 3, 3, 5]))
+
+#NO SE HACE
