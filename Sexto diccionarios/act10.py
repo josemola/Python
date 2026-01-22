76f52bb0418bd7c04229fe3f6f3a21cde159a66e
josemola <jsangal0411@ieszaidinvergeles.org>
2026-01-22 09:40:00 +0100

    10 arreglado

--- Sexto diccionarios/act10.py
+++ Sexto diccionarios/act10.py
@@ -52,9 +52,9 @@ while x!=6:
         for nifs, datoss in cliente.items():
             if datoss['preferencia']=="True":
                 print(f"NIF: {nifs} - Nombre: {datoss['nombre']}")
+    
     elif x==6:
         print("Adiós")
-    
     
     else :
         print("No te he entendido")
